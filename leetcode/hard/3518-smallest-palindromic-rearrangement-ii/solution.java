c597b20977f1636b6c1b0afa17a3b5bd94c3496f
SRIYANSH RAJ <173752326+sriyansh-dev@users.noreply.github.com>
2026-07-29 14:38:25 +0530

    ✅ Smallest Palindromic Rearrangement II | Hard | Java | Runtime: 20 ms (74.19%) | Memory: 47.6 MB | 2026-07-29

--- leetcode/hard/3518-smallest-palindromic-rearrangement-ii/solution.java
+++ leetcode/hard/3518-smallest-palindromic-rearrangement-ii/solution.java
@@ -1,15 +1,12 @@
 class Solution {
 
     long nCr(int n, int r, int k) {
-        //nCr == nC(n-r)
-        //5C3 == 5C2
-        //5C2 == 5C(5-2) = 5C3
-        r = Math.min(r, n - r); //nCr == nC(n-r)
+        r = Math.min(r, n - r); 
 
         long result = 1;
 
-        for (int i = 1; i <= r; i++) { //O(log2(k))
-            result = result * (n - r + i) / i; //result is becoming twice
+        for (int i = 1; i <= r; i++) { 
+            result = result * (n - r + i) / i; 
 
             if (result >= k)
                 return k;
@@ -22,17 +19,16 @@ class Solution {
         int n = s.length();
 
         char mid = ' ';
-        if (n % 2 == 1) { //odd length
+        if (n % 2 == 1) { 
             mid = s.charAt(n / 2);
         }
 
         int[] count = new int[26];
         for (int i = 0; i < n; i++) {
-            if (n % 2 == 1 && i == n / 2) continue; //mid character reserved for middle one
+            if (n % 2 == 1 && i == n / 2) continue; 
             count[s.charAt(i) - 'a']++;
         }
 
-        //half frequency will be used to build halfResult
         for (int i = 0; i < 26; i++) {
             count[i] /= 2;
         }
@@ -40,15 +36,12 @@ class Solution {
         StringBuilder halfResult = new StringBuilder();
         int half = n / 2;
 
-        for (int i = 0; i < half; i++) { //O(n/2)
-            //I am trying to fill ith position
-            //What if I could never fill a character in ith position
-            boolean placedCharacter = false; //in ith position
-            for (int j = 0; j < 26; j++) { //which character to put
+        for (int i = 0; i < half; i++) { 
+            boolean placedCharacter = false; 
+            for (int j = 0; j < 26; j++) { 
                 if (count[j] > 0) {
                     count[j] -= 1;
 
-                    //count number of ways
                     long ways = 1;
                     int letters = 0;
                     for (int c = 0; c < 26; c++) {
@@ -57,7 +50,7 @@ class Solution {
 
                     for (int c = 0; c < 26; c++) {
                         if (count[c] > 0) {
-                            ways *= nCr(letters, count[c], k); //log2(k)
+                            ways *= nCr(letters, count[c], k); 
                             letters -= count[c];
                         }
 
@@ -65,30 +58,24 @@ class Solution {
                             break;
                         }
                     }
-
-                    if (ways >= k) { //this block contains my kth one
-                        halfResult.append((char) (j + 'a')); //fixed this character at ith position
+                    if (ways >= k) { 
+                        halfResult.append((char) (j + 'a')); 
                         placedCharacter = true;
                         break;
                     }
 
-                    k -= ways; //when k >= ways
+                    k -= ways; 
                     count[j] += 1;
                 }
             }
-
             if (placedCharacter == false)
                 return "";
         }
-
-        //halfResult + mid + (reverse of halfResult)
         StringBuilder rev = new StringBuilder(halfResult);
-        rev.reverse(); //O(n/2)
-
+        rev.reverse(); 
         if (mid != ' ') {
             halfResult.append(mid);
         }
-
         return halfResult.toString() + rev.toString();
     }
 }
