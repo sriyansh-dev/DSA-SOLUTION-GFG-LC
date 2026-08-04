929a32867b1180fe2eae8af812749c8e2ca7ac56
SRIYANSH RAJ <173752326+sriyansh-dev@users.noreply.github.com>
2026-08-04 09:57:32 +0530

    ✅ Find Missing Elements | Easy | Java | Runtime: 3 ms (81.76%) | Memory: 46.9 MB | 2026-08-04

---
+++ leetcode/easy/3731-find-missing-elements/solution.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            set.add(num);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+
+        return result;
+    }
+}
