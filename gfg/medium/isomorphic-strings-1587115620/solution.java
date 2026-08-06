3133a68315b4a7cf3cb69739d4060e6545d7b9b4
SRIYANSH RAJ <173752326+sriyansh-dev@users.noreply.github.com>
2026-08-06 22:59:10 +0530

    ✅ isomorphic-strings-1587115620 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-06

---
+++ gfg/medium/isomorphic-strings-1587115620/solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean areIsomorphic(String s1, String s2) {
+        
+        //Terminate if the lengths are not same
+        if(s1.length() != s2.length())
+            return false;
+            
+        //Create a hashtable for lowercase character mapping(size = 26)
+        Hashtable<Character, Character> hash = new Hashtable<>(26);
+        
+        //Traverse through the strings
+        for(int i = 0;i < s1.length();i++){
+            char a = s1.charAt(i), b = s2.charAt(i);
+            
+            //If a is an unvisited character,
+            //and b is an unmapped character,
+            //map (a -> b)
+            //If b is an already mapped character,
+            //it can't be mapped to a so, return false.
+            if(!hash.containsKey(a)){
+                if(!hash.containsValue(b))
+                    hash.put(a, b);
+                else
+                    return false;
+            }
+            
+            //If a is a visited character,
+            //check if it's mapped character is actually b.
+            //If no, return false.
+            else{
+                if(hash.get(a) != b)
+                    return false;
+            }
+        }
+        
+        //If the strings survived the loop
+        return true;
+    }
+}
