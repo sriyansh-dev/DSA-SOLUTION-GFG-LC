1310627b165c61c94f66cdce0b2932879eca293e
SRIYANSH RAJ <173752326+sriyansh-dev@users.noreply.github.com>
2026-09-10 23:17:12 +0530

    ✅ possible-pairs1550 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-10

---
+++ gfg/medium/possible-pairs1550/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    virtual int gcd_cal(int a, int b){
+      if(b==0) return a;
+      return gcd_cal(b, a%b);
+    }
+    
+    virtual int lcm_cal(int a, int b){
+      return ((a*b)/(gcd_cal(a, b)));    
+    }
+  
+    virtual int pairCount(int x, int y){
+      int p=x*y;
+      
+      int sqrt_p=sqrt(p);
+      
+      int ans=0;
+      
+      for(int i=1; i<=sqrt_p; i++){
+       if(p%i==0){
+         int a=i, b=p/i;
+         if(gcd_cal(a, b)==x && lcm_cal(a, b)==y){
+          ans++;
+          if(a!=b) ans++;     
+         }
+       }   
+      }
+      
+      return ans;
+    }
+};
